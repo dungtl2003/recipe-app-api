d223b629eddadc90539b2dcfd1c7676a3c10d80d
ilikeblue <luudung0806@gmail.com>
2023-08-02 19:08:37 +0700

    fix exit code 1 error

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,18 +16,12 @@ class Command(BaseCommand):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
-        max_tries = 7
-        tried = 1
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2Error, OperationalError) as e:
-                if tried > max_tries:
-                    self.stdout.write(self.style.ERROR('Connection failed!'))
-                    raise e
-                self.stdout.write(f'Database unavailable, attempting to reconnect ({tried})')
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Database unavailable, reconnecting in 1 second...')
                 time.sleep(1)
-                tried += 1
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
