d7a92662a8a823eb1ee2f4c95a8033e1e88a7f2f
ilikeblue <luudung0806@gmail.com>
2023-08-03 14:58:01 +0700

    normalize user email

--- app/core/models.py
+++ app/core/models.py
@@ -18,7 +18,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_field):
         """Create, save and return a new user."""
         # self.model() is liked User()
-        user = self.model(email=email, **extra_field)
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         # support multiple databases.
         user.save(using=self._db)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,3 +25,16 @@ class ModelTest(TestCase):
         # password in user should be hashed, so we can not compare password
         # like email --> use check_password function.
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Tests email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.com', 'test4@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
