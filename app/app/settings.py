1d395997d792379461a866875d03d29c5f6c2123
ilikeblue <luudung0806@gmail.com>
2023-08-03 14:19:17 +0700

    add user model and user manager

--- app/app/settings.py
+++ app/app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,40 @@
+"""
+Database models.
+"""
+# AbstractBaseUser contains functionality for auth system, but not fields,
+# PermissionMixin contains functioinality for the permissions and fields
+# needed.
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user."""
+        # self.model() is liked User()
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        # support multiple databases.
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # assign user manager
+    objects = UserManager()
+
+    # define field for authentication, default is username, not email.
+    USERNAME_FIELD = 'email'
