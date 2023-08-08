43feaa48b950f3f54df24a628040e5986a7f259b
ilikeblue <luudung0806@gmail.com>
2023-08-08 19:50:59 +0700

    add image attr to recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,7 @@ Tests for models.
 # default user model. Also, we want to save user in test database, so we will
 # use TestCase rather than SimpleTestCase.
 from decimal import Decimal
+from unittest.mock import patch
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -95,3 +96,16 @@ class ModelTest(TestCase):
         ingredient = models.Ingredient.objects.create(user=user, name='Sample')
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    # uuid: unique identifier for the file we want to upload. This will
+    # ensure each file will have the unique name.
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """test generating image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        # This function is supposed to generate the path to the image that
+        # will be uploaded.
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
