e0adeeef577e62a3d9d820ed7ae65902f326c082
ilikeblue <luudung0806@gmail.com>
2023-08-09 17:49:18 +0700

    create docker compose production config

--- app/app/settings.py
+++ app/app/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': os.environ.get('DB_PORT')
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASS'),
+        'PORT': os.environ.get('POSTGRES_PORT')
     }
 }
 
